27a33274de3f2dad916e8125b907bb09ee4434c1
Sk_Sharujan <151716214+Sksharujan1108@users.noreply.github.com>
2024-09-20 14:11:08 +0530

    update

--- src/navigation/stack/authStack/authStack.tsx
+++ src/navigation/stack/authStack/authStack.tsx
@@ -16,7 +16,7 @@ const Stack = createNativeStackNavigator<AuthStackParamList>();
 const AuthStack = () => {
   return (
     <Stack.Navigator
-      initialRouteName = 'WelcomeScreen'
+      initialRouteName = 'LogInScreen'
       screenOptions = {{ headerShown: false }}
     >
         <Stack.Screen name = "SplashScreen" component = {Splash} />
